b0d9f8bd6ff6f537b91933e83adfd7868fcc8a82
Roman Schmid <rsc@hinderlingvolkart.com>
2020-07-13 12:01:02 +0200

    Make index.json wait for data…

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,16 +1,26 @@
 import posts from './_posts.js';
 
-const contents = JSON.stringify(posts.map(post => {
+const data = JSON.stringify(posts.map(post => {
 	return {
 		title: post.title,
 		slug: post.slug
 	};
 }));
 
-export function get(req, res) {
+async function getPosts() {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve(data);
+		}, 5000);
+	});
+}
+
+export async function get(req, res) {
 	res.writeHead(200, {
 		'Content-Type': 'application/json'
 	});
+
+	const contents = await getPosts();
 
 	res.end(contents);
 }
